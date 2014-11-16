e7433f2bd37422df6626c712297de735c6ed8e32
Nathan Moeller <ndmoeller@wisc.edu>
2014-11-16 15:16:58 -0600

    laser readings work

--- main.cpp
+++ main.cpp
@@ -2,28 +2,31 @@
 
 #include <iostream>
 using namespace std;
+
+
+
 int main(int argc, char **argv)
 {
 	Aria::init();
 	ArSimpleConnector connector(&argc, argv);
 	ArRobot robot;
 	ArSick sick;
+	int t, cnt;
+	cnt = 1;
 	double laser_dist[900];
 	double laser_angle[900];
 	std::list<ArSensorReading *> *readings;
 	std::list<ArSensorReading *>::iterator it;
-	int t, cnt;
+
+	ArKeyHandler keyHandler; 
+	Aria::setKeyHandler(&keyHandler);
+	robot.attachKeyHandler(&keyHandler);
 
 	if (!Aria::parseArgs() || argc > 1){
 		Aria::logOptions();
 		Aria::shutdown();
 		exit(1);
 	}
-
-	ArKeyHandler keyHandler;
-	Aria::setKeyHandler(&keyHandler);
-	robot.attachKeyHandler(&keyHandler);
-
 	// add the laser to the robot
 	robot.addRangeDevice(&sick);
 
@@ -38,44 +41,44 @@ int main(int argc, char **argv)
 	robot.runAsync(true);
 	puts("Robot Running");
 
-	// now set up the laser
-	connector.setupLaser(&sick); //this line throws an exception
+	//toggle true to false when actually connecting to the robot
+	sick.configureShort(true, ArSick::BAUD38400, ArSick::DEGREES180, ArSick::INCREMENT_ONE);
+	// set up the laser before handing it to the laser mode
+	connector.setupLaser(&sick); //this line throws an exception in debugging mode
+
 	sick.runAsync();
-	
-	if (!sick.blockingConnect())
-	{
+
+	if (!sick.blockingConnect()){
 		printf("Could not connect to SICK laser... exiting\n");
 		Aria::shutdown();
 		return 1;
 	}
-	else{
-		printf("connected to laser");
-	}
 
-	//get data from the sick laser scanner
+	
 	cnt = 1;
-	while (cnt < 10000){
-		readings = (list<ArSensorReading *, allocator<ArSensorReading *> > *)
-		sick.getRawReadings();//CurrentBuffer..
+	while (cnt<10000){
+		readings = (list<ArSensorReading *, allocator<ArSensorReading *> > *)sick.getRawReadings();//CurrentBuffer..
 		while (readings == NULL){
-			readings = (list<ArSensorReading *, allocator<ArSensorReading *> > *)
-			sick.getRawReadings();
+			readings = (list<ArSensorReading *, allocator<ArSensorReading *> > *)sick.getRawReadings();
 		}
 		t = 0;
 		for (it = readings->begin(); it != readings->end(); it++){
+			//cout << "t: " << t << endl;
 			laser_dist[t] = (*it)->getRange();
 			laser_angle[t] = -90 + t;
-			cout << "laser angle: " << laser_angle[t] << " laser dist.: " << laser_dist[t] <<" "<<"\n";
+			//cout << "laser angle: " << laser_angle[t] << " laser dist.: " << laser_dist[t] <<" "<<"\n";
 			t++;
 		}
+		cout << "count: " << cnt << endl; //for some reason this line needs to be here
 		cnt++;
 
 	}
-	/*
+	
 	for (t = 0; t<181; t++){
 		cout << "laser angle: " << laser_angle[t] << " laser dist.: " << laser_dist[t] << " " << "\n";
 	}
-	*/
+
+
 	/*
 	// turn on the motors, turn off amigobot sounds
 	robot.enableMotors();
@@ -84,9 +87,8 @@ int main(int argc, char **argv)
 	//this will set the robot to go in a straight line at a constant velocity (must set runAsync to true)
 	ArActionConstantVelocity constantVelocity("Constant Velocity", 400);
 	robot.addAction(&constantVelocity, 50);
-	// wait for robot task loop to end before exiting the program
-	
 	*/
+	// wait for robot task loop to end before exiting the program
 	robot.waitForRunExit();
 
 	/*
